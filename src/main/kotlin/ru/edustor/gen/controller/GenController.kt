666ddaf38f9dfac38249c01b061ffabf7d488e50
Dmitry Romanov <me@wutiarn.ru>
2016-11-24 16:31:56 +0300

    Page count and content type

--- src/main/kotlin/ru/edustor/gen/controller/GenController.kt
+++ src/main/kotlin/ru/edustor/gen/controller/GenController.kt
@@ -1,14 +1,16 @@
 package ru.edustor.gen.controller
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.edustor.gen.internal.PdfGenerator
-import java.io.OutputStream
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class GenController(val pdfGenerator: PdfGenerator) {
-    @RequestMapping("/")
-    fun root(outStream: OutputStream) {
-        pdfGenerator.makePdf(outStream, 10)
+    @RequestMapping("/", "/{pageCount}")
+    fun root(resp: HttpServletResponse, @PathVariable(required = false) pageCount: Int?) {
+        resp.setHeader("Content-Type", "application/pdf")
+        pdfGenerator.makePdf(resp.outputStream, pageCount ?: 1)
     }
 }
