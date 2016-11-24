4da67d547d09db235b9620a37d589cfc4e5f78a0
Dmitry Romanov <me@wutiarn.ru>
2016-11-24 21:15:49 +0300

    QR code

---
+++ src/main/kotlin/ru/edustor/gen/util/qr.kt
@@ -0,0 +1,27 @@
+package ru.edustor.gen.util
+
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+import java.awt.Image
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
+
+fun makeQR(content: String): BufferedImage {
+    val bitMatrix = MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 300, 300, mapOf(
+            EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.L,
+            EncodeHintType.MARGIN to 0
+    ));
+    val bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix)
+    return bufferedImage
+}
+
+fun Image.getAsByteArray(): ByteArray {
+    val outputStream = ByteArrayOutputStream()
+    ImageIO.setUseCache(false)
+    ImageIO.write(this as BufferedImage, "png", outputStream)
+    return outputStream.toByteArray()
+}
