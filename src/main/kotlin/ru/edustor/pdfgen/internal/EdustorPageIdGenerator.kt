51511189dd4b5d4a345c14f9b22476c060657a5b
Dmitry Romanov <me@wutiarn.ru>
2017-06-03 23:47:33 +0300

    WIP: Code 128

--- src/main/kotlin/ru/edustor/pdfgen/internal/EdustorPageIdGenerator.kt
+++ src/main/kotlin/ru/edustor/pdfgen/internal/EdustorPageIdGenerator.kt
@@ -1,18 +1,35 @@
 package ru.edustor.pdfgen.internal
 
 import org.springframework.stereotype.Component
+import java.time.Instant
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @Component
 open class EdustorPageIdGenerator {
+
+    private var lastEpochSecond = 0L
+    private var lastPageUid = 0
+
     fun getNewPageId(): PageId {
 
         val now = LocalDateTime.now()
-        val pageUid = "00000000"
+        val pageUid = getPageUid()
         val pageId = now.format(DateTimeFormatter.ofPattern("yyMMddHHmmss")) + pageUid
 
         return PageId(pageId)
+    }
+
+    private fun getPageUid(): String {
+        val epochSecond = Instant.now().epochSecond
+
+        if (epochSecond != lastEpochSecond) {
+            lastEpochSecond = epochSecond
+            lastPageUid = 0
+        }
+
+        val result = lastPageUid++.toString().padStart(8, '0')
+        return result
     }
 
     data class PageId(val id: String) {
